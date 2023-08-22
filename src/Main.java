12f4da3cca5a381ebd972cde9fba7b3715c8226d
JoeK <ohhjassassin@gmail.com>
2023-08-22 16:16:00 +0100

    Login system, case sensitive. Login is Admin and Password

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,35 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Scanner input
+        //Scanner package for input
+        Scanner input = new Scanner(System.in);
+
+        //Menu | Using .trim() to remove whitespace
+        System.out.print("Enter your username: ");
+        String username = input.nextLine().trim();
+
+        System.out.print("Enter your password: ");
+        String password = input.nextLine().trim();
+
+        //Try catch to catch any errors
+        try
+        {
+            //Must match exactly Admin and Password with no whitespace before or after and case sensitive
+            if (username.trim().equals("Admin") && password.trim().equals("Password"))
+            {
+                System.out.println("Welcome " + username);
+            }
+            else
+            {
+                throw new Exception("Incorrect username or password");
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("""
+                    ACCESS DENIED!
+                    Invalid Credentials
+                    The login is case sensitive""");
+        }
     }
 }
